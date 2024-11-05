cb4d389d0a2f08664683e8b12b181dea4a40e0b7
MahiBR <mrbathula02@gmail.com>
2024-11-05 17:45:10 -0500

    a practise file

---
+++ src/newfile/gitlab/Practise1.java
@@ -0,0 +1,11 @@
+package newfile.gitlab;
+
+public class Practise1 {
+	public static void main(String[] args) {
+		Practise1 i=new Practise1();
+		i.print();
+	}
+	void print() {
+		System.out.println("print this");
+	}
+}
