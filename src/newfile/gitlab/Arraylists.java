9f1f589ffc79ce3e23c53d8dd95ede08efa47e0e
MahiBR <mrbathula02@gmail.com>
2024-11-07 21:59:44 -0500

    added linkedlist

---
+++ src/newfile/gitlab/Arraylists.java
@@ -0,0 +1,17 @@
+package newfile.gitlab;
+import java.util.ArrayList;
+public class Arraylists {
+	public static void main(String args[]) {
+	ArrayList<String> u=new ArrayList<>();
+	u.add("hi");
+	u.add("hello");
+	u.add("this");
+	u.add("method");
+	u.add("is add");
+	System.out.println(u);
+	u.remove(1);
+	System.out.println(u);
+	
+	
+	}
+}
---
+++ src/newfile/gitlab/Linkedlist.java
@@ -0,0 +1,28 @@
+package newfile.gitlab;
+import java.util.LinkedList;
+public class Linkedlist {
+	public static void main(String[] args) {
+		LinkedList m=new LinkedList();
+			m.add("hello");
+			m.add("coffe");
+			m.add(10);
+			m.add(10.1f);
+			m.add("lets see");
+			m.add("lets remove");
+			m.add("this too");
+			System.out.println(m);
+			m.remove(4);
+			System.out.println(m);
+			m.removeLast();
+			System.out.println(m);
+			m.addLast("last one");
+			System.out.println(m);
+			m.addFirst(100);
+			System.out.println(m);
+			m.removeFirst();
+			System.out.println(m);
+			for(int a=0;a<=1;a++) {
+				System.out.println(m);
+			}
+	}
+}
